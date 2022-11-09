821df03a61a2093edd269fef0e8a20749781e2e0
Sai Kiran Challa <saikiranchalla4@gmail.com>
2022-11-09 09:38:56 -0700

    Add project files.

---
+++ AspNetCoreDemo/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreDemo.Data;
+using AspNetCoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // SELECT * FROM CATEGORIES;
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ AspNetCoreDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // Category model => Table Categories
+
+        // Create TABle...
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
